1f0079c2af02f1a48e1e341bb2986f86d47d6951
SytHamMer <mathys.lebon1@gmail.com>
2023-05-29 22:12:43 +0200

    [COMMENT]

--- WEB/main_page.php
+++ WEB/main_page.php
@@ -6,7 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel='stylesheet' href='main_page.css' />
     <title>Document</title>
+
+
 </head>
+<!-- Nous n'avons pas réussi a trouver une solution pour faire fonctionner le contrat sur MetaMask ou Etherum
+Ce qui nous a donc empéché de faire la suite, c'est pourquoi nous avons tout de même réalisé cette page 
+sans pour autant essayer de la relier car de toute facon le contrat n'était pas utilisable  -->
 <body>
     <div id="fond">
         <div id="topbar">
